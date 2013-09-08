d399aa3406d5064c50cba9fc93f3fe6a3690930b
Jestan Nirojan <jestannirojan@gmail.com>
2013-09-08 17:38:46 +0530

    Integrated accelerometer input - improvised algorithm to guide the snake :). - added different colors for food, snake and game score.

--- html5-snake-app/platforms/android/assets/www/cordova_plugins.js
+++ html5-snake-app/platforms/android/assets/www/cordova_plugins.js
@@ -1,3 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = []
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.core.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.core.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    }
+]
 });
